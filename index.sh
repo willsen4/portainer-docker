7c3588efa4263c7906b2bf65274d0e735c5840bc
William de Oliveira Sena <willsenaa@hotmail.com>
2025-08-04 10:01:48 -0300

    Update index.sh

--- index.sh
+++ index.sh
@@ -14,26 +14,3 @@ YAML
 Bash
 docker compose up -d
 
-#Acesse a Interface Web do Portainer
-#Depois que o contêiner estiver em execução, você poderá acessar a interface web do Portainer no seu navegador.
-#Abra seu navegador e vá para:
-#https://localhost:9443 (se você estiver acessando do mesmo servidor onde o Docker está rodando).
-#Ou https://[IP_DO_SEU_SERVIDOR]:9443 (substitua [IP_DO_SEU_SERVIDOR] pelo endereço IP do seu servidor).
-#Na primeira vez que você acessar, será solicitado que você crie um usuário e uma senha de administrador para o Portainer.
-
-#Comandos Úteis:
-#Para parar o Portainer:
-Bash
-docker compose stop
-
-#Para parar e remover o Portainer (e seus volumes, se quiser um recomeço limpo):
-Bash
-docker compose down
-
-#Para remover também o volume de dados (cuidado, isso apaga as configurações do Portainer!):
-Bash
-docker compose down -v
-
-#Para ver o status dos contêineres:
-Bash
-docker compose ps
